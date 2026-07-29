3ac497e0fc2111289f9194a6c8cabf1812ed7600
klarak777 <klarak777@github.com>
2026-07-30 01:48:56 +0200

    Fix migration: make is_inactive column addition idempotent

--- server/src/db/migrations/20260730000000_041_add_inactive_to_identifiers.js
+++ server/src/db/migrations/20260730000000_041_add_inactive_to_identifiers.js
@@ -3,9 +3,12 @@
  * Inactive identifiers are shown in partner editor but excluded from Admin module dropdowns.
  */
 exports.up = async function(knex) {
-  await knex.schema.alterTable('partner_identifiers', table => {
-    table.boolean('is_inactive').defaultTo(false).notNullable();
-  });
+  const hasColumn = await knex.schema.hasColumn('partner_identifiers', 'is_inactive');
+  if (!hasColumn) {
+    await knex.schema.alterTable('partner_identifiers', table => {
+      table.boolean('is_inactive').defaultTo(false).notNullable();
+    });
+  }
 };
 
 exports.down = async function(knex) {
